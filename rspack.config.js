0eb22a8560317817c07f664b35ad10d19601c0de
Antony Fagundez <jhonfagundez@gmail.com>
2023-03-24 21:12:46 -0300

    basic example

--- rspack.config.js
+++ rspack.config.js
@@ -31,28 +31,6 @@ const config = {
     ]),
   ], // Copy the files from the public directory to the dist directory, aligning the behavior of the CRA
 
-  // module:{
-  //   rules: [
-  //     {
-  //       test: /\.module\.css$/,
-  //       use: [
-  //         {
-  //           loader: 'postcss-loader',
-  //           options: {
-  //             postcssOptions: {
-  //               plugins: [
-  //                 [
-  //                   "postcss-preset-env",
-  //                 ],
-  //               ],
-  //             },
-  //           },
-  //         },
-  //       ],
-  //       type: 'css',
-  //     },
-  //   ],
-  // }
 };
 
 module.exports = config;
