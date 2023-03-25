dda55daa6b3c9be5de279a26cfb49833405ccd53
Antony Fagundez <jhonfagundez@gmail.com>
2023-03-24 21:01:52 -0300

    fix: initial migration working on linux

--- rspack.config.js
+++ rspack.config.js
@@ -1,17 +1,26 @@
 const CopyPlugin = require('copy-webpack-plugin');
+
+
 /**
- * @type {import('@rspack/cli').Configuration}
- */
-module.exports = {
+* @type {import('@rspack/cli').Configuration}
+*/
+const config = {
   entry: {
-    main: './src/index.jsx', // Configure the project entry file
+    main: './src/index.tsx', // Configure the project entry file
   },
   builtins: {
     html: [
       {
-        template: './index.html', // Align CRA to generate index.html
+        template: './public/index.html', // Align CRA to generate index.html
+        files: {
+          favicon: "./favicon.ico",
+          manifest: "./manifest.json",
+        }
       },
     ],
+    
+
+
   },
   plugins: [
     new CopyPlugin([
@@ -21,4 +30,29 @@ module.exports = {
       },
     ]),
   ], // Copy the files from the public directory to the dist directory, aligning the behavior of the CRA
+
+  // module:{
+  //   rules: [
+  //     {
+  //       test: /\.module\.css$/,
+  //       use: [
+  //         {
+  //           loader: 'postcss-loader',
+  //           options: {
+  //             postcssOptions: {
+  //               plugins: [
+  //                 [
+  //                   "postcss-preset-env",
+  //                 ],
+  //               ],
+  //             },
+  //           },
+  //         },
+  //       ],
+  //       type: 'css',
+  //     },
+  //   ],
+  // }
 };
+
+module.exports = config;
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {coso} from "./App.module.css"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p className={coso}>
+          Example of sass
         </p>
         <a
           className="App-link"
