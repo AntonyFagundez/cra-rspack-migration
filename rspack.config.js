5fcf8c5b329462edc78c28228d855100e879518b
Antony Fagundez <jhonfagundez@gmail.com>
2023-04-09 21:27:47 -0300

    feat: configuration ready for production

--- rspack.config.js
+++ rspack.config.js
@@ -1,36 +1,89 @@
-const CopyPlugin = require('copy-webpack-plugin');
 
-
+const path = require("path");
+const envVars = require("./environment.config");
 /**
-* @type {import('@rspack/cli').Configuration}
-*/
-const config = {
-  entry: {
-    main: './src/index.tsx', // Configure the project entry file
-  },
-  builtins: {
-    html: [
-      {
-        template: './public/index.html', // Align CRA to generate index.html
-        files: {
-          favicon: "./favicon.ico",
-          manifest: "./manifest.json",
+ * 
+ * @returns {import('@rspack/cli').Configuration}
+ */
+module.exports = function(env, argv) {
+  
+  const isProd = process.env.NODE_ENV === "production";
+  
+  return {
+    entry: {
+      main: './src/index.tsx', // Configure the project entry file
+    },
+    stats: isProd ? "normal" : "errors-only",
+    mode: isProd ? "production" : "development",
+    devtool: isProd ? false : "source-map",
+    devServer: {
+      port: process.env.PORT || 3000,
+      webSocketServer: "sockjs",
+      historyApiFallback: true,
+      compress: true,
+      client: {
+        overlay: true,
+        progress: false,
+      },
+    },
+    builtins: {
+      html: [
+        {
+          template: './index.html', // Align CRA to generate index.html
+        },
+      ],
+      copy: {
+        patterns: [{
+          from: "public",
+          to: "."
+        }]
+      },
+      css: {
+        modules: {
+          localIdentName: isProd
+            ? `${process.env.npm_package_name}_[name]_[local]`
+            : `${process.env.npm_package_name}_[path][name]__[local]`,
+        },
+      },
+      define: {
+        "process.env": envVars
+      },
+    },
+    resolve: { 
+      modules: ["node_modules"], 
+      alias: {
+        "~": path.resolve(__dirname, "src"),
+      } 
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: ["sass-loader"],
+          type: "css",
+        },
+        {
+          test: /\.module\.scss$/,
+          use: ["sass-loader"],
+          type: "css/module",
+        },
+        {
+          resourceQuery: /raw/,
+          type: 'asset/source'
         }
+      ]
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          reactVendor: {
+            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+            name: "vendor-react",
+            chunks: "all",
+            priority: -10,
+          },
+        },
       },
-    ],
-    
-
-
-  },
-  plugins: [
-    new CopyPlugin([
-      {
-        from: 'public',
-        to: '.',
-      },
-    ]),
-  ], // Copy the files from the public directory to the dist directory, aligning the behavior of the CRA
-
+    },
+  }
 };
-
-module.exports = config;
